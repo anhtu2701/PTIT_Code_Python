056613b345eaa2ae0498af150c8437d3a77d6446
htu.coder <anhtugaming2701@gmail.com>
2025-01-31 00:51:36 +0700

    Update PY02008

--- PY02008_KHOẢNG_CÁCH_NGUYÊN_TỐ.py
+++ PY02008_KHOẢNG_CÁCH_NGUYÊN_TỐ.py
@@ -1,13 +1,13 @@
 def sang(n):
     a = [True] * (n+1)
     a[0] = a[1] = False
-    for i in range(2, n+1):
+    for i in range(2, int(n**0.5)+1):
         if a[i]:
             for j in range(i*i, n+1, i):
                 a[j] = False
     return [i for i in range(2, n+1) if a[i]]
 n, x = map(int, input().split())
-prime = sang(1000)
+prime = sang(10000)
 arr = prime[:n]
 lst = [x]
 for i in arr:
